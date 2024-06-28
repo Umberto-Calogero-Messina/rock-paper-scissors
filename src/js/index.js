79d6403250c868bdadc18ff8e030dac487a2dedc
Genezis <ucmessina@alumnostrazos.net>
2024-06-29 01:28:43 +0200

    +Update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,157 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+/* 
+    - Localizar los elementos implicados del DOM
+    - Crear los datos del programa necesarios
+
+FLUJO DEL PROGRAMA
+    - Detectar dónde hemos click
+    - Guardar nuestra jugada
+    - Generar una jugada aleatoria para el ordenador y guardarla
+    - Comparar jugadas
+    - Mostrar resultado
+    - Asignar puntos
+*/
+
+const gameItemElement = document.getElementById('game-items');
+const userPointElement = document.getElementById('points-user');
+const PcPointElement = document.getElementById('points-pc');
+const resultElement = document.getElementById('game-result');
+const resetGameElement = document.getElementById('play-again');
+const userPickedElement = document.getElementById('picked-user-image');
+const pcPickedElement = document.getElementById('picked-pc-image');
+const resultShowElement = document.getElementById('game-results');
+
+let userPoint = 0;
+let pcPoint = 0;
+//let userPlay;
+let pcPlay;
+
+const pcPlaySimpleArray = ['rock', 'paper', 'scissors'];
+const pcPlayadvanceArray = ['rock', 'paper', 'scissors', '', ''];
+
+console.dir(pcPlaySimpleArray);
+
+const symbolObject = {
+  rock: ['TIE', 'LOSE', 'WIN'],
+  paper: ['WIN', 'TIE', 'LOSE'],
+  scissors: ['LOSE', 'WIN', 'TIE']
+};
+
+// const userObject = {
+//   rock: 0,
+//   paper: 1,
+//   scissors: 2
+// };
+
+const imgObject = {
+  rock: '../assets/images/icon-rock.svg',
+  paper: '../assets/images/icon-paper.svg',
+  scissors: '../assets/images/icon-scissors.svg'
+};
+
+const checkWinner = event => {
+  // If user = Pc = tie
+
+  //console.log('user = ' + userPlay);
+  //console.log('pc = ' + pcPlay);
+  // if (userPlay === pcPlay) {
+  //   resultElement.textContent = 'TIE';
+  //   //console.log('enter');
+  //   return;
+  // }
+
+  // symbolObject -> pasa valor ordenador y me dice si user ha perdido
+
+  //console.log(symbolObject[userPlay][pcPlay] + '---');
+
+  //console.log(symbolObject[userPlay][pcPlay -1]);
+
+  // console.log(event + 'user');
+  // console.log(pcPlay + 'pc');
+  // console.log(symbolObject[event][pcPlay]);
+
+  if (symbolObject[event][pcPlay] === 'TIE') {
+    resultElement.textContent = symbolObject[event][pcPlay];
+    return;
+  }
+  if (symbolObject[event][pcPlay] === 'WIN') {
+    resultElement.textContent = symbolObject[event][pcPlay];
+    userPoint++;
+    return;
+  }
+
+  if (symbolObject[event][pcPlay] === 'LOSE') {
+    resultElement.textContent = symbolObject[event][pcPlay];
+    pcPoint++;
+    return;
+  }
+
+  //console.log(symbolObject[userPlay][pcPlay]);
+
+  //else if user > pc = Win userPoint++
+  //else if user < px = Lose pcPoint++
+};
+
+const updateRanking = () => {
+  //Update ranking user & pc
+  userPointElement.textContent = userPoint;
+  PcPointElement.textContent = pcPoint;
+};
+
+const changeImgElement = (playerImg, pcImg) => {
+  //console.dir(userPickedElement);
+  //.src
+  //Change Img for both --> Dont work src patch
+  userPickedElement.src = imgObject[playerImg];
+  pcPickedElement.src = imgObject[pcImg];
+
+  console.log(pcImg);
+  //console.log(imgObject[userPlay]);
+  //console.log(imgObject[pcPlay]);
+};
+
+const generateRandomPcPlay = event => {
+  // Add lenth de algo para añadir luego más jugadas
+
+  //Add if to check file.index
+  //   if ()
+  //     else()
+  const randomValue = Math.floor(Math.random() * pcPlaySimpleArray.length);
+
+  pcPlay = randomValue;
+  // console.log(pcPlaySimpleArray[pcPlay]);
+
+  checkWinner(event.target.dataset.item);
+  ShowRankingWindow();
+  updateRanking();
+  changeImgElement(event.target.dataset.item, pcPlaySimpleArray[pcPlay]);
+};
+
+const startGame = event => {
+  if (!event.target.classList.contains('game-item')) return;
+  //console.log(event.target.dataset.item);
+
+  //userPlay = userObject[event.target.dataset.item];
+  //console.log(userObject[userPlay]);
+  generateRandomPcPlay(event);
+};
+
+const resetGame = () => {
+  //console.log('ResetGame');
+  resultElement.textContent = '';
+  HideRankingWindow();
+};
+
+const ShowRankingWindow = () => {
+  resultShowElement.classList.remove('hide');
+  gameItemElement.classList.add('hide');
+};
+const HideRankingWindow = () => {
+  resultShowElement.classList.add('hide');
+  gameItemElement.classList.remove('hide');
+};
+
+gameItemElement.addEventListener('click', startGame);
+resetGameElement.addEventListener('click', resetGame);
